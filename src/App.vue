c635463e31ff1e77e642452bbe4ede08594db17d
Valentine Orlovskiy <orloval@uqt.gr>
2026-01-12 17:05:21 +0200

    Fix: moved navigation to separate component

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,16 @@
 <template>
     <div class="app">
-        <nav class="nav">
-            <router-link to="/index">
-                Both chats
-            </router-link>
-            <router-link to="/rest-chat">
-                Rest chat
-            </router-link>
-            <router-link to="/websocket-chat">
-                Websocket chat
-            </router-link>
-        </nav>
+        <Nav />
 
         <main class="main">
-            <router-view/>
+            <router-view />
         </main>
     </div>
 </template>
 
 <script setup lang="ts">
+import Nav from '@/components/Nav.vue';
+
 import { useChatStore } from '@/stores/chat'
 
 useChatStore().loadMessagesFromStorage()
@@ -27,36 +19,6 @@ useChatStore().loadMessagesFromStorage()
 <style scoped>
 .app {
     font-family: Arial, sans-serif;
-}
-
-.nav {
-    display: block;
-    position: fixed;
-    left: 0;
-    top: 0;
-    width: 160px;
-    height: 100vh;
-    gap: 20px;
-    margin-bottom: 30px;
-    padding: 16px;
-    background: #E6E6E6;
-}
-
-.nav a {
-    display: block;
-    text-decoration: none;
-    color: #333;
-    font-weight: bold;
-    padding: 8px 16px;
-    border-radius: 4px;
-    transition: background 0.2s;
-    margin-bottom: 16px;
-}
-
-.nav a:hover,
-.nav a.router-link-active {
-    background: #007bff;
-    color: white;
 }
 
 .main {
---
+++ src/components/Nav.vue
@@ -0,0 +1,45 @@
+<template>
+    <nav class="nav">
+        <router-link to="/index">
+            Both chats
+        </router-link>
+        <router-link to="/rest-chat">
+            Rest chat
+        </router-link>
+        <router-link to="/websocket-chat">
+            Websocket chat
+        </router-link>
+    </nav>
+</template>
+
+<style scoped>
+.nav {
+    display: block;
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 160px;
+    height: 100vh;
+    gap: 20px;
+    margin-bottom: 30px;
+    padding: 16px;
+    background: #E6E6E6;
+}
+
+.nav a {
+    display: block;
+    text-decoration: none;
+    color: #333;
+    font-weight: bold;
+    padding: 8px 16px;
+    border-radius: 4px;
+    transition: background 0.2s;
+    margin-bottom: 16px;
+}
+
+.nav a:hover,
+.nav a.router-link-active {
+    background: #007bff;
+    color: white;
+}
+</style>
